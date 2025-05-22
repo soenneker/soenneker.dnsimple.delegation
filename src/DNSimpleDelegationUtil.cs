b5c1630dd84f8714405f1c9054fe107cee749978
Jake Soenneker <jake@soenneker.com>
2025-05-21 19:28:03 -0500

    misc

--- src/DNSimpleDelegationUtil.cs
+++ src/DNSimpleDelegationUtil.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Soenneker.DNSimple.OpenApiClient;
+using Soenneker.DNSimple.OpenApiClient.Item.Registrar.Domains.Item.Delegation;
+using Soenneker.DNSimple.OpenApiClient.Item.Registrar.Domains.Item.Delegation.Vanity;
 using Soenneker.DNSimple.OpenApiClient.Models;
+using Soenneker.DNSimple.OpenApiClientUtil.Abstract;
+using Soenneker.Extensions.Configuration;
+using Soenneker.Extensions.Task;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Soenneker.DNSimple.OpenApiClient.Item.Registrar.Domains.Item.Delegation;
-using Soenneker.DNSimple.OpenApiClient.Item.Registrar.Domains.Item.Delegation.Vanity;
-using Soenneker.DNSimple.OpenApiClientUtil.Abstract;
-using Soenneker.Extensions.Configuration;
+using Soenneker.Extensions.ValueTask;
 
 public sealed class DNSimpleDelegationUtil : IDNSimpleDelegationUtil
 {
@@ -23,34 +25,34 @@ public sealed class DNSimpleDelegationUtil : IDNSimpleDelegationUtil
 
     public async ValueTask<List<string>?> ListNameServers(string domain, CancellationToken cancellationToken = default)
     {
-        DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken);
-        DelegationGetResponse? response = await client[_accountId].Registrar.Domains[domain].Delegation.GetAsDelegationGetResponseAsync(cancellationToken: cancellationToken);
+        DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken).NoSync();
+        DelegationGetResponse? response = await client[_accountId].Registrar.Domains[domain].Delegation.GetAsDelegationGetResponseAsync(cancellationToken: cancellationToken).NoSync();
         return response?.Data;
     }
 
     public async ValueTask<List<string>?> ChangeNameServers(string domain, List<string> nameServers, CancellationToken cancellationToken = default)
     {
-        DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken);
+        DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken).NoSync();
         DelegationPutResponse? response = await client[_accountId]
                                                 .Registrar.Domains[domain]
-                                                .Delegation.PutAsDelegationPutResponseAsync(nameServers, cancellationToken: cancellationToken);
+                                                .Delegation.PutAsDelegationPutResponseAsync(nameServers, cancellationToken: cancellationToken).NoSync();
         return response?.Data;
     }
 
     public async ValueTask<List<NameServer>?> DelegateToVanityNameServers(string domain, List<string> nameServers,
         CancellationToken cancellationToken = default)
     {
-        DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken);
+        DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken).NoSync();
         VanityPutResponse? response = await client[_accountId]
                                             .Registrar.Domains[domain]
-                                            .Delegation.Vanity.PutAsVanityPutResponseAsync(nameServers, cancellationToken: cancellationToken);
+                                            .Delegation.Vanity.PutAsVanityPutResponseAsync(nameServers, cancellationToken: cancellationToken).NoSync();
         return response?.Data;
     }
 
     public async ValueTask<bool> DedelegateFromVanityNameServers(string domain, CancellationToken cancellationToken = default)
     {
-        DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken);
-        await client[_accountId].Registrar.Domains[domain].Delegation.Vanity.DeleteAsync(cancellationToken: cancellationToken);
+        DNSimpleOpenApiClient client = await _clientUtil.Get(cancellationToken).NoSync();
+        await client[_accountId].Registrar.Domains[domain].Delegation.Vanity.DeleteAsync(cancellationToken: cancellationToken).NoSync();
         return true;
     }
 }
